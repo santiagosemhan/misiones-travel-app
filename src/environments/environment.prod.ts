6abb067d7885a24389f2433c10b63ab2575243e1
Matias Solis de la Torre <matias.delatorre89@gmail.com>
2021-06-01 16:05:30 -0300

    remove enviroments

--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'https://misiones-travel.herokuapp.com',
-  mapbox: 'pk.eyJ1IjoibWlzaW9uZXN0cmF2ZWwiLCJhIjoiY2tpZzNoZXlkMGEwaDJyczU1MDZhZThzdSJ9.TEpoxSF0t5lH0r2z9A_g6w'
-};
